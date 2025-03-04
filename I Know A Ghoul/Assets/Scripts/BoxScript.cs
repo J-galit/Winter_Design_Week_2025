c2e92c25ef22e6a650412a7a975d4341d5e7a80e
RoyalRedBird <33841152+RoyalRedBird@users.noreply.github.com>
2025-03-04 14:24:30 -0500

    Test is fully operational.

--- I Know A Ghoul/Assets/Scripts/BoxScript.cs
+++ I Know A Ghoul/Assets/Scripts/BoxScript.cs
@@ -16,6 +16,7 @@ public class BoxScript : MonoBehaviour
 
         diagRunner = GameObject.FindObjectOfType<DialogueRunner>();
         diagRunner.AddFunction<bool>("get_spoken_to_player", getSpokenToPlayer);
+        diagRunner.AddFunction<bool>("flag_box_as_player_spoken", flagAsSpokenToPlayer);
 
     }
 
@@ -32,12 +33,13 @@ public class BoxScript : MonoBehaviour
 
     }
 
-    [YarnCommand ("flag_box_as_player_spoken")]
-    public void flagAsSpokenToPlayer()
+    public bool flagAsSpokenToPlayer()
     {
 
         spokenToPlayer = true;
         Debug.Log("The box has spoken to the player.");
+
+        return true;
 
     }
 
