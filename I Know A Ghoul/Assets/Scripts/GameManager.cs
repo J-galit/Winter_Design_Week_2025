0bcf9350a1bc91294360647bffa493db407795d4
RoyalRedBird <33841152+RoyalRedBird@users.noreply.github.com>
2025-03-05 16:42:43 -0500

    Text input nearly complete, just need the Yarn to recognise it on its end.

--- I Know A Ghoul/Assets/Scripts/GameManager.cs
+++ I Know A Ghoul/Assets/Scripts/GameManager.cs
@@ -1,31 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using Yarn.Unity;
 
 public class GameManager : MonoBehaviour
 {
 
     DialogueRunner diagRunner;
+    string userInput = "";
+    public TextMeshProUGUI textField;
+    [SerializeField] GameObject textBox;
+
+    public InMemoryVariableStorage varStorage;
+
 
     // Start is called before the first frame update
     void Awake()
     {
 
         diagRunner = GameObject.FindObjectOfType<DialogueRunner>();
+        diagRunner.AddFunction<string>("get_player_input", getUserInput);
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     public void SpeakToBox()
     {
 
         diagRunner.StartDialogue("BoxScript");
+
+    }
+
+    [YarnCommand("user_text_prompt")]
+    public IEnumerator userInputPrompt()
+    {
+        Debug.Log("enter coroutine");
+        textBox.SetActive(true);
+
+        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space) == true);
+
+        if (textField.text.ToString() != "")
+        {
+            Debug.Log("second if accessed");
+            userInput = textField.text.ToString().ToLower();
+            textField.text = null;
+            textBox.SetActive(false);
+            yield return null;
+
+        }
+        
+        Debug.Log("end of coroutine");
+    }
+
+    public string getUserInput()
+    {
+
+        Debug.Log("userInput is " + userInput);
+        return userInput;
 
     }
 
