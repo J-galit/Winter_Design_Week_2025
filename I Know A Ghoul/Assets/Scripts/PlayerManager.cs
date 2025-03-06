719a3fd13800b435e8bb042a5020da0706a206c9
J-galit <jaygalita@gmail.com>
2025-03-06 14:44:15 -0500

    Finished Sigma and Roxie

--- I Know A Ghoul/Assets/Scripts/PlayerManager.cs
+++ I Know A Ghoul/Assets/Scripts/PlayerManager.cs
@@ -9,6 +9,8 @@ public class PlayerManager : MonoBehaviour
     DialogueRunner diagRunner;
 
     [SerializeField] bool hasThing;
+    [SerializeField] bool visitSpot;
+    [SerializeField] bool talkedToBouncer;
 
     // Start is called before the first frame update
     void Awake()
@@ -16,6 +18,9 @@ public class PlayerManager : MonoBehaviour
 
         diagRunner = GameObject.FindObjectOfType<DialogueRunner>();
         diagRunner.AddFunction<bool>("get_player_has_thing", getHasThing);
+        diagRunner.AddFunction<bool>("did_player_visit_spot", getDidPlayerVisitSpot);
+        diagRunner.AddFunction<bool>("getTalkedToBouncer", getTalkedToBouncer);
+        diagRunner.AddFunction<bool>("TalkedToBouncer", TalkedToBouncer);
         
 
     }
@@ -24,6 +29,25 @@ public class PlayerManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+
+    //Photographer Functions
+    public bool getDidPlayerVisitSpot()
+    {
+        return visitSpot;
+    }
+
+    //Bouncer Functions
+    public bool getTalkedToBouncer()
+    {
+        return talkedToBouncer;
+    }
+
+    public bool TalkedToBouncer()
+    {
+        talkedToBouncer = true;
+        return true;
     }
 
     public bool getHasThing()
@@ -39,5 +63,8 @@ public class PlayerManager : MonoBehaviour
         hasThing = true;
 
     }
+
+   
+
 
 }
