229e2b356c0af1834ca7b4e9ad667c99f3313258
J-galit <jaygalita@gmail.com>
2025-03-06 19:45:56 -0500

    Added a restart Button

--- I Know A Ghoul/Assets/Scripts/EndSceneMeni.cs
+++ I Know A Ghoul/Assets/Scripts/EndSceneMeni.cs
@@ -5,22 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class EndSceneMeni : MonoBehaviour
 {
-    public void loadMainGame()
-    {
-
-        SceneManager.LoadScene("MenuScene");
-
-    }
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("MenuScene");
+
+        }
     }
+   
 }
